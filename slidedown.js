6044d82ced958abd8d6fd29083db9706a7309350
wellsjohn220 <60244641+wellsjohn220@users.noreply.github.com>
2025-04-07 11:38:45 +1000

    Update slidedown.js

--- slidedown.js
+++ slidedown.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 const xValues = ["China","Australia","Italy", "France", "Spain", "USA", "Argentina", "Japan"];
 const yValues = [30, 28, 25, 20, 18, 15, 13, 11];
-const barColors = ["yellow","red", "green","blue","orange","brown","grey","pink"];
+const barColors = ["yellow","red", "green","blue","orange","brown","grey","lime"];
 
 new Chart("myChart", {
   type: "bar",
